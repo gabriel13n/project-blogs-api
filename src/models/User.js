0e55316fc35658544371c90c3d3acb62f40a081e
Gabriel Cantuária <gabriel13n@gmail.com>
2023-01-03 22:14:52 -0300

    requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.INTEGER
+    },
+    image: {
+      type: DataTypes.STRING,
+    }, 
+  }, {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users'
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Blogpost, {
+      as: 'posts',
+      foreignKey: 'user_id'
+    });
+  }
+
+  return User;
+};
+
+module.exports = User;
