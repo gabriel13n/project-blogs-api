c626eae97567063e1872befc6c4d4d29280b9139
Gabriel Cantuária <gabriel13n@gmail.com>
2023-01-04 20:31:35 -0300

    requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    }
+  }, {
+    timestamps: false, 
+    underscored: true,
+    tableName: 'categories'
+  });
+
+  return Category;
+}
+
+module.exports = Category;
